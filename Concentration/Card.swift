a66488f103f2097e804628c39f50b155823d6af6
Raul Lopez <areacode.code@gmail.com>
2018-03-22 20:14:59 -0700

    Second Project Version

---
+++ Concentration/Card.swift
@@ -0,0 +1,28 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Raul on 3/20/18.
+//  Copyright © 2018 Monkey Wrench Labs. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    private static var identifierFactory = 0
+    
+    private static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1 // 1 to make it new id
+        return Card.identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
+
